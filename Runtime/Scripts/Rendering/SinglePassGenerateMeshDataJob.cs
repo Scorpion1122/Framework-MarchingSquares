001ff0d369acbc2f805b10ce7227dc43dd8f411f
Scorpion1122 <thijs.kooyers@gmail.com>
2019-11-22 16:58:41 +0100

    wip efficient mesh generation

---
+++ Runtime/Scripts/Rendering/SinglePassGenerateMeshDataJob.cs
@@ -0,0 +1,283 @@
+using System;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+namespace Thijs.Framework.MarchingSquares
+{
+    public struct VertexCache : IDisposable
+    {
+        public NativeArray<int> prevRow;
+        public NativeArray<int> midRow;
+        public NativeArray<int> curRow;
+
+        public VertexCache(int resolution)
+        {
+            // prev and cur also needs to store the vertex between voxels, thats wy resolution * 2
+            prevRow = new NativeArray<int>(resolution * 2, Allocator.Temp);
+            midRow = new NativeArray<int>(resolution, Allocator.Temp);
+            curRow = new NativeArray<int>(resolution * 2, Allocator.Temp);
+        }
+
+        public void Swap()
+        {
+            NativeArray<int> temp = prevRow;
+            prevRow = curRow;
+            curRow = temp;
+        }
+
+        public void Dispose()
+        {
+            prevRow.Dispose();
+            midRow.Dispose();
+            curRow.Dispose();
+        }
+    }
+
+    public struct SinglePassGenerateMeshDataJob : IJob
+    {
+        [ReadOnly] public int resolution;
+        [ReadOnly] public float size;
+        [ReadOnly] public NativeArray<FillType> generateForFillTypes;
+        [ReadOnly] public NativeArray<FillType> fillTypes;
+        [ReadOnly] public NativeArray<float2> offsets;
+
+        public NativeList<float2> vertices;
+        public NativeList<int> triangleIndices;
+        public NativeList<int> triangleLengths;
+
+        public void Execute()
+        {
+            VertexCache cache = new VertexCache(resolution);
+
+            for (int i = 0; i < generateForFillTypes.Length; i++)
+            {
+                Execute(generateForFillTypes[i], cache);
+            }
+            cache.Dispose();
+        }
+
+        private void Execute(FillType fillType, VertexCache cache)
+        {
+            for (int i = 0; i < fillTypes.Length; i++)
+            {
+                //Last voxel of a row
+                if (i % resolution == resolution - 1)
+                {
+                    cache.Swap();
+                    return;
+                }
+
+                Execute(i, fillType, cache);
+            }
+        }
+
+        private void Execute(int index, FillType fillType, VertexCache cache)
+        {
+            int topIndex = index + resolution;
+            int topRightIndex = index + resolution + 1;
+            int rightIndex = index + 1;
+
+            FillType currentFill = fillTypes[index];
+            FillType topFill = GetNeightbourFillType(topIndex);
+            FillType topRightFill = GetNeightbourFillType(topRightIndex);
+            FillType rightFill = GetNeightbourFillType(rightIndex);
+
+            int voxelType = VoxelUtility.GetVoxelShape(
+                fillType,
+                currentFill,
+                topFill,
+                topRightFill,
+                rightFill);
+
+            if (voxelType == 0)
+                return;
+
+            float2 curPosition = VoxelUtility.IndexToPosition(index, resolution, size);
+            float2 topPosition = VoxelUtility.IndexToPosition(topIndex, resolution, size);
+            float2 topRightPosition = VoxelUtility.IndexToPosition(topRightIndex, resolution, size);
+            float2 rightPosition = VoxelUtility.IndexToPosition(rightIndex, resolution, size);
+
+            float2 currentOffset = offsets[index];
+            float2 topOffset = GetNeightbourOffset(topIndex);
+            float2 rightOffset = GetNeightbourOffset(rightIndex);
+
+            bool first = index % resolution == 0 || index < resolution;
+            int curCacheI = (index % resolution) * 2;
+            int midCacheI = index % resolution;
+
+            int v1, v2, v3, v4, v5, v6;
+            switch (voxelType)
+            {
+                //None
+                case 0:
+                    break;
+
+                //One Corner
+                case 1:
+                    if (first)
+                    {
+                        vertices.Add(curPosition);
+                        vertices.Add(curPosition + new float2(0, currentOffset.y));
+                        vertices.Add(curPosition + new float2(currentOffset.x, 0));
+
+                        v1 = vertices.Length - 3;
+                        v2 = vertices.Length - 2;
+                        v3 = vertices.Length - 1;
+                    }
+                    else
+                    {
+                        v1 = cache.prevRow[curCacheI];
+                        v2 = cache.midRow[midCacheI];
+                        v3 = cache.prevRow[curCacheI + 1];
+                    }
+
+                    AddOneCornerPolygon(v1, v2, v3);
+
+                    cache.curRow[curCacheI] = v1;
+                    cache.midRow[midCacheI] = v2;
+                    cache.curRow[curCacheI + 1] = v3;
+                    break;
+                case 2:
+                    AddOneCornerPolygon(
+                        fillType,
+                        topPosition,
+                        topPosition + new float2(topOffset.x, 0),
+                        curPosition + new float2(0, currentOffset.y));
+                    break;
+                case 4:
+                    AddOneCornerPolygon(
+                        fillType,
+                        topRightPosition,
+                        rightPosition + new float2(0, rightOffset.y),
+                        topPosition + new float2(topOffset.x, 0));
+                    break;
+                case 8:
+                    AddOneCornerPolygon(
+                        fillType,
+                        rightPosition,
+                        curPosition + new float2(currentOffset.x, 0),
+                        rightPosition + new float2(0, rightOffset.y));
+                    break;
+
+                //Two Corners
+                case 3:
+                    AddTwoCornerPolygon(
+                        fillType,
+                        curPosition,
+                        topPosition,
+                        topPosition + new float2(topOffset.x, 0),
+                        curPosition + new float2(currentOffset.x, 0));
+                    break;
+                case 6:
+                    AddTwoCornerPolygon(
+                        fillType,
+                        topPosition,
+                        topRightPosition,
+                        rightPosition + new float2(0, rightOffset.y),
+                        curPosition + new float2(0, currentOffset.y));
+                    break;
+                case 12:
+                    AddTwoCornerPolygon(
+                        fillType,
+                        topRightPosition,
+                        rightPosition,
+                        curPosition + new float2(currentOffset.x, 0),
+                        topPosition + new float2(topOffset.x, 0));
+                    break;
+                case 9:
+                    AddTwoCornerPolygon(
+                        fillType,
+                        rightPosition,
+                        curPosition,
+                        curPosition + new float2(0, currentOffset.y),
+                        rightPosition + new float2(0, rightOffset.y));
+                    break;
+
+                //Opposite Corners
+                case 5:
+                    AddCrossCornerPolygon(
+                        fillType,
+                        curPosition,
+                        curPosition + new float2(0, currentOffset.y),
+                        curPosition + new float2(currentOffset.x, 0),
+                        topRightPosition,
+                        rightPosition + new float2(0, rightOffset.y),
+                        topPosition + new float2(topOffset.x, 0));
+                    break;
+                case 10:
+                    AddCrossCornerPolygon(
+                        fillType,
+                        topPosition,
+                        topPosition + new float2(topOffset.x, 0),
+                        curPosition + new float2(0, currentOffset.y),
+                        rightPosition,
+                        curPosition + new float2(currentOffset.x, 0),
+                        rightPosition + new float2(0, rightOffset.y));
+                    break;
+
+                //Three Corners
+                case 7:
+                    AddThreeCornersPolygon(
+                        fillType,
+                        topPosition,
+                        topRightPosition,
+                        rightPosition + new float2(0, rightOffset.y),
+                        curPosition + new float2(currentOffset.x, 0),
+                        curPosition);
+                    break;
+                case 14:
+                    AddThreeCornersPolygon(
+                        fillType,
+                        topRightPosition,
+                        rightPosition,
+                        curPosition + new float2(currentOffset.x, 0),
+                        curPosition + new float2(0, currentOffset.y),
+                        topPosition);
+                    break;
+                case 13:
+                    AddThreeCornersPolygon(
+                        fillType,
+                        rightPosition,
+                        curPosition,
+                        curPosition + new float2(0, currentOffset.y),
+                        topPosition + new float2(topOffset.x, 0),
+                        topRightPosition);
+                    break;
+                case 11:
+                    AddThreeCornersPolygon(
+                        fillType,
+                        curPosition,
+                        topPosition,
+                        topPosition + new float2(topOffset.x, 0),
+                        rightPosition + new float2(0, rightOffset.y),
+                        rightPosition);
+                    break;
+
+                //All Corners
+                case 15:
+                    AddAllCornerPolygon(
+                        fillType,
+                        curPosition,
+                        topPosition,
+                        topRightPosition,
+                        rightPosition);
+                    break;
+            }
+        }
+
+        private FillType GetNeightbourFillType(int index)
+        {
+            if (index >= fillTypes.Length)
+                return FillType.None;
+            return fillTypes[index];
+        }
+
+        private float2 GetNeightbourOffset(int index)
+        {
+            if (index >= offsets.Length)
+                return float2.zero;
+            return offsets[index];
+        }
+    }
+}
