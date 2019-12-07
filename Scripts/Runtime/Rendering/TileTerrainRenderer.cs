190bebf62db663d4c7c8586d23677ae05b869745
Scorpion1122 <thijs.kooyers@gmail.com>
2019-12-07 13:49:03 +0100

    first working version of shadows

--- Scripts/Runtime/Rendering/TileTerrainRenderer.cs
+++ Scripts/Runtime/Rendering/TileTerrainRenderer.cs
@@ -1,14 +1,40 @@
+using System;
+using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Thijs.Framework.MarchingSquares
 {
     [ExecuteInEditMode]
     public class TileTerrainRenderer : TileTerrainComponent
     {
+        private static readonly int tileTerrainBlitTarget = Shader.PropertyToID("_TileTerrainBlitTarget");
+        private static readonly int screenCopy = Shader.PropertyToID("_ScreenCopy");
+        private static readonly int blurBlitTargetOne = Shader.PropertyToID("_BlurBlitTargetOne");
+        private static readonly int blurBlitTargetTwo = Shader.PropertyToID("_BlurBlitTargetTwo");
+        private static readonly CameraEvent SHADOW_CAMERA_EVENT = CameraEvent.AfterEverything;
+        
+        private List<ChunkRenderer> renderers = new List<ChunkRenderer>();
+        private List<Camera> cameras = new List<Camera>();
+        
+        [SerializeField] private Material shadowMaskMaterial;
+        [SerializeField] private Material blitMaterial;
+        [SerializeField] private Material blurMaterial;
+
+        private CommandBuffer shadowCommandBuffer;
+        private bool renderesChanged;
+        
         private void OnEnable()
         {
             TileTerrain.OnChunkInitialized += OnChunkInitialized;
+        }
+
+        private void OnDisable()
+        {
+            TileTerrain.OnChunkInitialized -= OnChunkInitialized;
+            ClearCommandBuffers();
+            
         }
 
         private void OnChunkInitialized(int2 chunkIndex, ChunkData chunkData)
@@ -20,11 +46,82 @@ namespace Thijs.Framework.MarchingSquares
 
             ChunkRenderer chunkRenderer = gameObject.AddComponent<ChunkRenderer>();
             chunkData.dependencies.Add(chunkRenderer);
+            renderers.Add(chunkRenderer);
+
+            renderesChanged = true;
         }
 
-        private void OnDisable()
+        private void LateUpdate()
         {
-            TileTerrain.OnChunkInitialized -= OnChunkInitialized;
+            if (renderesChanged)
+            {
+                UpdateCameraBuffers();
+                renderesChanged = false;
+            }
+        }
+
+        private void UpdateCameraBuffers()
+        {
+            ClearCommandBuffers();
+            
+            shadowCommandBuffer = CreateShadowCommandBuffer();
+            CameraUtility.GetActiveCameras(ref cameras);
+            for (int i = 0; i < cameras.Count; i++)
+            {
+                cameras[i].AddCommandBuffer(SHADOW_CAMERA_EVENT, shadowCommandBuffer);
+            }
+        }
+
+        private void ClearCommandBuffers()
+        {
+            if (shadowCommandBuffer == null) 
+                return;
+            
+            for (int i = 0; i < cameras.Count; i++)
+            {
+                if (cameras[i] != null)
+                    cameras[i].RemoveCommandBuffer(SHADOW_CAMERA_EVENT, shadowCommandBuffer);
+            }
+            cameras.Clear();
+        }
+
+        private CommandBuffer CreateShadowCommandBuffer()
+        {
+            CommandBuffer buffer = new CommandBuffer();
+            buffer.name = "Tile Terrain Shadows";
+            
+            buffer.GetTemporaryRT(tileTerrainBlitTarget, -1, -1, 0);
+            buffer.GetTemporaryRT(screenCopy, -1, -1, 0);
+            buffer.GetTemporaryRT(blurBlitTargetOne, -1, -1, 0);
+            buffer.GetTemporaryRT(blurBlitTargetTwo, -1, -1, 0);
+            
+            buffer.Blit(BuiltinRenderTextureType.CameraTarget, screenCopy);
+            buffer.SetGlobalTexture("_ScreenCopy", screenCopy);
+
+            // Render terrain to temp RT target
+            buffer.SetRenderTarget(tileTerrainBlitTarget);
+            buffer.ClearRenderTarget(true, true, Color.clear);
+            for (int i = 0; i < renderers.Count; i++)
+            {
+                if (renderers[i] != null)
+                    buffer.DrawRenderer(renderers[i].MeshRenderer, shadowMaskMaterial);
+            }
+
+            buffer.Blit(tileTerrainBlitTarget, blurBlitTargetOne, blurMaterial, 0);
+            buffer.Blit(blurBlitTargetOne, blurBlitTargetTwo, blurMaterial, 1);
+            
+            buffer.Blit(blurBlitTargetTwo, BuiltinRenderTextureType.CameraTarget, blitMaterial);
+            //buffer.Blit(tileTerrainBlitTarget, BuiltinRenderTextureType.CameraTarget, blitMaterial);
+
+            buffer.ReleaseTemporaryRT(tileTerrainBlitTarget);
+            buffer.ReleaseTemporaryRT(screenCopy);
+            buffer.ReleaseTemporaryRT(blurBlitTargetOne);
+            buffer.ReleaseTemporaryRT(blurBlitTargetTwo);
+            
+            buffer.SetRenderTarget(BuiltinRenderTextureType.CameraTarget);
+            //buffer.ClearRenderTarget(true, true, Color.red);
+
+            return buffer;
         }
     }
 }
---
+++ Shaders/AlphaBlit.shader
@@ -0,0 +1,56 @@
+﻿Shader "Unlit/AlphaBlit"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags {"Queue" = "Transparent" "RenderType"="Transparent" } 
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _ScreenCopy;
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);                
+                fixed4 screenCol = tex2D(_ScreenCopy, float2(i.uv.x, 1 - i.uv.y));
+                return screenCol * (1 - col.r);//lerp(screenCol, col, col.a);
+            }
+            ENDCG
+        }
+    }
+}
